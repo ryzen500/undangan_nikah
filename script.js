7dd5508e7cfb2efaf616c17a171cbf28c41cdad1
ryzen500 <ahmadtsani145@gmail.com>
2024-05-11 00:30:35 +0700

    Fetch With API post

--- script.js
+++ script.js
@@ -24,5 +24,69 @@ function updateCountdown() {
     document.getElementById("seconds").innerText = seconds;
 }
 
+function Simpan(){
+    
+
+    var formData = {
+        nama_pengirim: jQuery('#nama').val(),
+        ucapan: jQuery('#cui-textarea-17372').val(),
+        konfirmasi: jQuery('#konfirmasi').val()
+    };
+
+    jQuery.ajax({
+        type: 'POST',
+        url: 'http://localhost:12345/api/reservation/messages/post',
+        contentType: 'application/json',
+        data: JSON.stringify(formData),
+        success: function (response) {
+             // Buat elemen <li> untuk ditambahkan ke <ul>
+             var newComment = `
+             <li class="comment even thread-even depth-1 cui-item-comment" data-likes="0">
+               <div class="cui-comment cui-clearfix">
+                 <!-- Avatar -->
+                 <div class="cui-comment-avatar">
+                   <!-- Bisa tambahkan gambar jika ada -->
+                 </div>
+                 
+                 <!-- Pesan -->
+                 <div class="cui-comment-content">
+                   <!-- Info -->
+                   <div class="cui-comment-info">
+                     <a title="${response.nama_pengirim}" class="cui-commenter-name">${response.nama_pengirim}</a>
+                     <span class="cui-post-author-mark cui-post-author-hadir">${response.konfirmasi}</span>
+                   </div>
+                   
+                   <!-- Ucapan -->
+                   <div class="cui-comment-text">
+                     <p>${response.ucapan}</p>
+                   </div>
+                   
+                   <!-- Tindakan -->
+                   <div class="cui-comment-actions">
+                     <span class="cui-comment-time">
+                       <i class="far fa-clock">
+                         Beberapa detik lalu
+                       </i>
+                     </span>
+                   </div>
+                 </div>
+               </div>
+             </li>
+           `;
+
+           console.log(response);
+           // Tambahkan elemen <li> ke <ul>
+           jQuery('#cui-container-comment-17372').append(newComment);
+        },
+        error: function (error) {
+            console.error('Error:', error);
+            alert('Terjadi kesalahan saat menyimpan reservasi.');
+        }
+    });
+
+
+
+
+}
 // Call updateCountdown every second to keep the countdown updated
 setInterval(updateCountdown, 1000);
